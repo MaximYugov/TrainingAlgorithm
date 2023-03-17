32de07f60b7e05c889c0d52feb36e484712dd003
Maxim Yugov <78841025+MaximYugov@users.noreply.github.com>
2023-03-17 11:57:28 +0300

    Create function.php

---
+++ arrays/array_split_even_odd/function.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Function returns new array with unsigned integers divided into separate groups (arrays).
+ * Condition of separation is whether it odd or even
+ * 
+ * @param array $array List of unsigned integers
+ * @return array List of arrays containing grouped integers
+ */
+function array_split_even_odd(array $array): array
+{
+    $result = [];
+
+    $index = 0;
+    $previousIsInSameGroup = true;
+
+    for ($i = 0; $i < count($array); $i++) {
+        if ($i > 0) {
+            $previousIsInSameGroup = ($array[$i] % 2 == $array[$i - 1] % 2) ? true : false;
+        }
+        if ($previousIsInSameGroup) {
+            $result[$index][] = $array[$i];
+        } else {
+            $index++;
+            $result[$index][] = $array[$i];
+        }
+    }
+
+    return $result;
+}
