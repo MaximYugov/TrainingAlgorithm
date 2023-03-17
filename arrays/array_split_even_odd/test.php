133c3dbd31cb6a4b30234357a2c79e631eef04e4
Maxim Yugov <78841025+MaximYugov@users.noreply.github.com>
2023-03-17 12:00:35 +0300

    Create test.php

---
+++ arrays/array_split_even_odd/test.php
@@ -0,0 +1,14 @@
+require_once __DIR__ . "/function.php";
+
+function assertEquals(mixed $actual, mixed $expected): void
+{
+    echo $actual == $expected ? "PASSED" : "FAILED", PHP_EOL;
+}
+
+assertEquals(array_split_even_odd([5, 7, 1, 6, 4, 8, 10]), [[5, 7, 1], [6, 4, 8, 10]]);
+
+assertEquals(array_split_even_odd([5, 2, 7, 1, 6, 4, 8, 10]), [[5], [2], [7, 1], [6, 4, 8, 10]]);
+
+assertEquals(array_split_even_odd([2, 6, 5, 4]), [[2, 6], [5], [4]]);
+
+assertEquals(array_split_even_odd([2, 6, 5, 0, 4]), [[2, 6], [5], [0, 4]]);
