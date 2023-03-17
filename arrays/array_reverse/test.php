61bdfef271ad9044b952688efdb89f037737703f
Maxim Yugov <78841025+MaximYugov@users.noreply.github.com>
2023-03-17 12:23:19 +0300

    Create test.php

---
+++ arrays/array_reverse/test.php
@@ -0,0 +1,12 @@
+<?php
+
+function assertEquals(mixed $actual, mixed $expected): void
+{
+    echo $actual == $expected ? "PASSED" : "FAILED", PHP_EOL;
+}
+
+assertEquals(array_create_reversed([3, 4, 86, 6, 78, 7]), [7, 78, 6, 86, 4, 3]);
+
+$source = [3, 4, 86, 6, 78, 7, "", 1, "s"];
+array_reverse_reference($source);
+assertEquals($source, ["s", 1, "", 7, 78, 6, 86, 4, 3]);
